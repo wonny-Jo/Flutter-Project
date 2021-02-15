4d55bb2daed906840f62f1b62e90f22e3720fa34
wonny-Jo <60210088+wonny-Jo@users.noreply.github.com>
2021-02-15 15:57:43 +0900

    Add files via upload

---
+++ snack_bar/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('snack bar'),
+          centerTitle: true,
+        ),
+        body: Builder(
+          builder: (BuildContext ctx) {
+            return Center(
+              child: FlatButton(
+                child: Text(
+                  'Show me',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.red,
+                onPressed: () {
+                  Scaffold.of(ctx).showSnackBar(SnackBar(
+                    content: Text('Hellow'),
+                  ));
+                },
+              ),
+            );
+          },
+        ));
+  }
+}
