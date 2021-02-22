fbe99c073280523712fa3f13491ac33858902868
wonny-Jo <60210088+wonny-Jo@users.noreply.github.com>
2021-02-22 16:25:41 +0900

    Add files via upload

---
+++ login_page/lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login app',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('sign in',
+        style: TextStyle(
+          color: Colors.white,
+        ),),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ButtonTheme(
+              height: 50.0,
+              child: RaisedButton(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Image.asset('images/glogo.png'),
+                    Text(
+                      'Login with Google',
+                      style: TextStyle(color: Colors.black87,
+                      fontSize: 15.0
+                      ),
+                    ),
+                    Opacity(
+                      opacity: 0.0,
+                        child: Image.asset('images/glogo.png'),
+                    ),
+                  ],
+                ),
+                  color: Colors.white,
+                  onPressed: (){}
+              ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(4.0),
+              ),
+            ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            ButtonTheme(
+              height: 50.0,
+              child: RaisedButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Image.asset('images/flogo.png'),
+                      Text(
+                        'Login with Facebook',
+                        style: TextStyle(color: Colors.white,
+                            fontSize: 15.0
+                        ),
+                      ),
+                      Opacity(
+                        opacity: 0.0,
+                        child: Image.asset('images/flogo.png'),
+                      ),
+                    ],
+                  ),
+                  color: Color(0xFF334D92),
+                  onPressed: (){}
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(4.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            ButtonTheme(
+              height: 50.0,
+              child: RaisedButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Icon(
+                        Icons.mail,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Login with Email',
+                        style: TextStyle(color: Colors.white,
+                            fontSize: 15.0
+                        ),
+                      ),
+                      Opacity(
+                        opacity: 0.0,
+                        child: Icon(
+                          Icons.mail,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  color: Colors.green,
+                  onPressed: (){}
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(4.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
