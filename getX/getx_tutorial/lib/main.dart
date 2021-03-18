25f4f78764a41c6a716014c9593e21bf09f98049
wonny-Jo <60210088+wonny-Jo@users.noreply.github.com>
2021-03-18 15:22:46 +0900

    Update main.dart

--- getX/getx_tutorial/lib/main.dart
+++ getX/getx_tutorial/lib/main.dart
@@ -18,6 +18,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      // home : Home(),
+      
       // initialRoute: '/',
       // routes: {
       //   '/': (context)=>Home(),
